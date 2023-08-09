03ec87f5aef4cf30a56c105c5b7c22139c9621e0
Kshitij.Osource <kshitij.osource@winadmins-MacBook-Air.local>
2023-08-09 15:25:31 +0530

    added functionality to parse flags and commands in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::i32;
+
 
 use serde::Deserialize;
 use serde::Deserializer;
@@ -12,6 +14,12 @@ use std::fs;
 pub enum DatabaseType {
     Postgres,
     MySql,
+}
+
+impl Default for DatabaseType {
+    fn default() -> Self {
+        DatabaseType::Postgres
+    }
 }
 
 impl<'de> Deserialize<'de> for DatabaseType {
@@ -37,10 +45,10 @@ struct ConfigFileInput {
     database_url: Option<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Config {
-    database: DatabaseType,
-    database_url: String,
+    pub database: DatabaseType,
+    pub database_url: String,
 }
 
 impl Config {
@@ -73,12 +81,127 @@ pub fn read_config_file() -> Result<Config, Box<dyn Error>> {
     return Ok(Config::new(db, db_url));
 }
 
-pub async fn ping_db(mut pool: &Pool<Postgres>) -> Result<(), Box<dyn Error>> {
+pub async fn ping_db(pool: &Pool<Postgres>) -> Result<(), Box<dyn Error>> {
     let result = sqlx::query("SELECT 1 + 1 as sum").fetch_one(pool).await?;
 
-    let s:i32 = result.get("sum");
-
-    dbg!(s);
+    let _s: i32 = result.get("sum");
 
     Ok(())
+}
+
+pub async fn table_exists(pool: &Pool<Postgres>) -> Result<bool, Box<dyn Error>> {
+    let table_exits_sql = "SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        where  table_name = 'db_migrations'
+        );";
+
+    let results = sqlx::query(table_exits_sql).fetch_one(pool).await?;
+    let tb_exists: bool = results.get("exists");
+
+    dbg!(tb_exists);
+
+    Ok(tb_exists)
+}
+
+pub async fn create_migration_table(pool: &Pool<Postgres>) -> Result<(), Box<dyn Error>> {
+    let create_table_sql = "create table db_migrations(
+        id serial primary key,
+      name text unique,
+      valid bool not null,
+      created_at timestamp not null DEFAULT now(),
+        deleted_at timestamp not null
+    );";
+
+    sqlx::query(create_table_sql).execute(pool).await?;
+
+    Ok(())
+}
+#[derive(Debug)]
+pub enum Command {
+    Up(bool, i32),
+    Down(i32),
+    New(String),
+}
+impl Default for Command {
+    fn default() -> Self {
+        Command::New(String::default())
+    }
+}
+
+#[derive(Default)]
+pub struct Flags {
+    pub config: Config,
+    pub cmd: Command,
+}
+
+impl Flags {
+    pub fn parse(args: Vec<String>) -> Result<Flags, Box<dyn Error>> {
+        let mut f: Flags = Flags::default();
+        let mut i = 1;
+        while i < args.len() {
+            match args[i].as_str() {
+                "-u" | "--db-url" => {
+                    if i + 1 < args.len() {
+                        f.config.database_url = args[i + 1].clone();
+                        i = i + 1
+                    }
+                }
+                "-d" | "--db" => {
+                    if i + 1 < args.len() {
+                        f.config.database_url = args[i + 1].clone();
+                        i = i + 1
+                    }
+                }
+                "new" => {
+                    if i + 1 < args.len() {
+                        let mig_name = args[i + 1].clone();
+                       
+
+                        f.cmd = Command::New(mig_name);
+                        return Ok(f);
+                    } else {
+                        return Err("please mention the name of the migration file")?;
+                    }
+                }
+                "up" => {
+                    if i + 1 < args.len() {
+                        match args[i + 1].clone().parse::<i32>() {
+                            Ok(n) => {
+                                f.cmd = Command::Up(false, n);
+                                return Ok(f);
+                            }
+                            Err(_) => {
+                                return Err("please enter a valid numeric value for up command")?;
+                            }
+                        }
+                    } else {
+                        f.cmd = Command::Up(true, -1);
+                    }
+                }
+                "down" => {
+                    if i + 1 < args.len() {
+                        match args[i + 1].clone().parse::<i32>() {
+                            Ok(n) => {
+                                f.cmd = Command::Down(n);
+                                return Ok(f);
+                            }
+                            Err(_) => {
+                                return Err("please enter a valid numeric value for down command")?;
+                            }
+                        }
+                    } else {
+                        return Err("please enter a valid numeric value for down command")?;
+                    }
+                }
+
+                _ => {
+                    return Err("invalid command")?;
+                }
+            }
+
+            i = i + 1;
+        }
+
+        return Ok(f);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,55 @@
-
 use std::error::Error;
+
 use std::{env, process};
 
-use migratour::{ping_db, read_config_file};
+use migratour::{create_migration_table, Flags, ping_db, read_config_file, table_exists};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let config = read_config_file().unwrap_or_else(|err| {
-        eprintln!("error reading file {}", err);
+    if args.len() < 2 {
+        eprintln!("please pass some argument");
+        process::exit(1)
+    }
+
+    let mut f = Flags::parse(args).unwrap_or_else(|err| {
+        eprintln!("error parsing flags{}", err);
         process::exit(1);
     });
 
-    let mut pool = sqlx::postgres::PgPool::connect("postgres://kshitij.360one:sVTezMu4E8YG@ep-shy-king-58645115.ap-southeast-1.aws.neon.tech/neondb?sslmode=require").await?;
+    if f.config.database_url == "" {
+        f.config = read_config_file().unwrap_or_else(|err| {
+            eprintln!("error reading file {}", err);
+            process::exit(1);
+        });
+    }
 
-    
+    dbg!(f.cmd);
+    let  pool = sqlx::postgres::PgPool::connect("postgres://kshitij.360one:sVTezMu4E8YG@ep-shy-king-58645115.ap-southeast-1.aws.neon.tech/neondb?sslmode=require").await?;
 
-    ping_db(&pool).await.unwrap_or_else(|err|{
+    ping_db(&pool).await.unwrap_or_else(|err| {
         eprintln!("error connecting to the database {}", err);
         process::exit(1);
     });
 
-    dbg!(config);
+    let tb_exists = table_exists(&pool).await.unwrap_or_else(|err| {
+        eprintln!("error connecting to database {}", err);
+        process::exit(1);
+    });
+
+    if !tb_exists {
+        create_migration_table(&pool).await.unwrap_or_else(|err| {
+            eprintln!("error creating database migration table {}", err);
+            process::exit(1);
+        })
+    }
+
+    // let mg_folder_exists = Path::new("./migrations").is_dir();
+
+    // if !mg_folder_exists{
+
+    // }
 
     Ok(())
 }
